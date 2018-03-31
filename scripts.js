e46f11cdd86f289b726c73cdbea1d76079a3f4e1
Goska <36965568+goskanowak@users.noreply.github.com>
2018-03-31 02:31:16 +0200

    change variable

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
 var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-
 var dino = 'triceratops';
-
 var dinoUpperCased = dino.toUpperCase();
 
 console.log(dinoUpperCased);
@@ -10,9 +8,8 @@ var changeVelociraptor = text.replace('Velociraptor', dinoUpperCased);
 
 console.log(changeVelociraptor);
 
-console.log(text.length/2);
-
-var partText = changeVelociraptor.slice(0,72);
+var getHalfSentence = (text.length / 2);
+var partText = changeVelociraptor.slice(0, getHalfSentence);
 
 document.getElementById('sentence').innerHTML = partText;
 
